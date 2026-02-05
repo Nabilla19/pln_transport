f96fdee4bf61ec3303445b80c0319d555cdecffb
Nia Nabilla <nia@pln.local>
2026-02-05 13:14:22 +0700

    yaallah fix ini

--- app/request/RequestFormClient.js
+++ app/request/RequestFormClient.js
@@ -160,7 +160,7 @@ export default function RequestFormClient() {
 
     return (
         <Shell>
-            <div className="min-h-screen p-4 md:p-8 lg:p-12 bg-white font-primary">
+            <div className="min-h-screen p-4 pt-8 md:p-8 lg:p-12 bg-white font-primary">
                 <div className="max-w-4xl mx-auto">
                     <div className="flex items-center justify-between mb-8">
                         <div>
